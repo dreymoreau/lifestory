21c5d18d6db33c13c1e7edde2f127299b230dbb5
dreymotoh <striclyspoken@gmail.com>
2023-05-23 10:52:32 -0400

    changes

--- client/src/components/Add.js
+++ client/src/components/Add.js
@@ -5,9 +5,10 @@ import { useNavigate } from 'react-router-dom';
 
 const Add = () => {
 
+  // default state before inputted info
   const [book, setBook] = useState ({
-    title:"",
-    desc:"",
+    title: "",
+    desc: "",
     cover: "",
     date: ""
   });
@@ -15,6 +16,7 @@ const Add = () => {
   const navigate = useNavigate()
 
   const handleChange = (e) => {
+    // to set the key values for book object
     setBook((prev) => ({...prev, [e.target.name]: e.target.value}))
   };
 
@@ -35,7 +37,7 @@ const Add = () => {
        <h1>Add new Book</h1>
         <input type='text' placeholder='title' onChange={handleChange} name='title'/>
         <input type='text' placeholder='desc' onChange={handleChange} name='desc'/>
-        <input type='text' placeholder='cover' onChange={handleChange} name='cover'/>
+        <input type='file' placeholder='cover' onChange={handleChange} name='cover'/>
         <input type='date' placeholder='date' onChange={handleChange} name='date'/>
       </div>
 

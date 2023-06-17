8dcb44c6373ec019bee5a0701f88cbb017569f40
dreymotoh <striclyspoken@gmail.com>
2023-06-17 12:47:23 -0400

    added multer to get images file working, not working yet

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const multer = require('multer')
 const app = express()
+const path = require('path')
 const dotenv = require('dotenv')
 const PORT = 8880
 
@@ -11,7 +13,12 @@ dotenv.config();
 // express middleware
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: true,
+    methods: ['GET', 'POST'],
+    credentials: true
+}))
+app.use('/', express.static(path.join(__dirname, '/')));
 
 // if there is a auth problem
 app.get('/', (req, res) => {
@@ -23,6 +30,25 @@ app.get('/books', (req,res) => {
     db.query(q, (err, data) => {  
         if (err) return res.json(err)
         return res.json(data)
+    })
+})
+
+app.get('/api/image', (req,res) => {
+
+    const id = 1
+    const sqlInsert = "SELECT * FROM images WHERE id = ?;"
+    connection.query(sqlInsert, [id] , (err, result) => {
+        if(err) {
+            console.log(err)
+            res.send({
+                msg: err
+            })
+        }
+        if(result) {
+            res.send({
+                image: result[0].image
+            })
+        }
     })
 })
 
@@ -41,6 +67,30 @@ app.post('/books', (req,res) => {
     })
 })
 
+app.post('/api/image', upload.single('image'), (req, res, err) => {
+    if (!req.file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        res.send({ msg:'Only image files (jpg, jpeg, png) are allowed!'});
+    } else {
+        const image = req.file.filename;
+        const id = 1
+        const sqlInsert = "UPDATE images SET `image` = ? WHERE id = ?;"
+        connection.query(sqlInsert, [image, id] , (err, result) => {
+            if(err) {
+                console.log(err)
+                res.send({
+                    msg: err
+                })
+            }
+            if(result) {
+                res.send({
+                    data: result,
+                    msg: 'Your image has been updated'
+                })
+            }
+    })
+    }
+})
+
 app.delete('/books/:id', (req, res)=> {
     const bookId = req.params.id;
     const q = 'DELETE FROM books WHERE id = ?'
@@ -49,6 +99,18 @@ app.delete('/books/:id', (req, res)=> {
         if (err) return res.json(err);
         return res.json("book has been deleted successfully")
     })
+})
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null, './');},
+        filename: function(req,file,cb){
+            const ext = file.mimetype.split('/')[1]
+            cb(null, `uploads/${file.originalname}-${Date.now()}.${ext}`);
+        }
+})
+const upload = multer({
+    storage: storage
 })
 app.listen(PORT, () => {
     console.log(`connected to backend port ${PORT}!`)
--- client/src/components/Add.js
+++ client/src/components/Add.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+const [uploadStatus, setUploadStatus] = useState('');
+
 
 const Add = () => {
 
@@ -30,6 +32,49 @@ const Add = () => {
     }
   }
 
+  const getImage = () => {
+  const [image, setImage] = useState('');
+  useEffect(() => {
+    fetch(`http://localhost:8880/api/image`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json, charset=UTF-8',
+        'Accept': 'application/json, text/html'
+      }, 
+      credentials: 'include'
+    })
+    .then(data => data.json())
+    .then((data) => {
+      console.log(data)
+      setImage('http://localhost:8880' + data.image)
+      console.log(image)
+    })
+  })
+  }
+
+
+  const imageHandler = (event) => {
+    const file = event.target.files[0]
+    const formData = new FormData()
+    formData.append('image', file)
+
+    fetch(`http://localhost://8880/api/image`,{
+      method: "POST",
+      body: formData,
+      headers: {
+        'Accept': 'multipart/form-data'
+      },
+      credentials: 'include'
+    })
+    .then(res => res.json())
+    .then(res => {
+      setUploadStatus(res.msg)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  }
+
   console.log(book)
   return (
     <div className="flex flex-col h-screen bg-gradient-to-b from-green-800 to-blue-600 p-4 text-black">
@@ -37,7 +82,8 @@ const Add = () => {
        <h1 className='flex justify-center align-center'>Add new Book</h1>
         <input type='text' placeholder='title' onChange={handleChange} name='title'className='form flex p-4 justify-center align-center mx-auto my-10 '/>
         <input type='text' placeholder='author' onChange={handleChange} name='desc'className='form flex p-4 justify-center align-center mx-auto my-10'/>
-        <input type='file' placeholder='cover' onChange={handleChange} name='cover' className='form flex p-4 justify-center align-center mx-auto my-10'/>
+        <h2> {uploadStatus} </h2>
+        <input type='file' placeholder='cover' accept='image/*' multiple={false} onChange={imageHandler} name='cover' className='form flex p-4 justify-center align-center mx-auto my-10'/>{image && <img src={image} alt="img"/>}
         <input type='date' placeholder='date' onChange={handleChange} name='date' className='form flex p-4 justify-center align-center mx-auto my-10'/>
       </div>
 
